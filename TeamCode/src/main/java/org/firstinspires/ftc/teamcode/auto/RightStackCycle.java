5b61cc83b41f1ffcb6b3c0ade3b045da080ca1c5
LiamLime_SR <wschilling003@gmail.com>
2023-01-23 17:02:09 -0500

    right auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RightStackCycle.java
@@ -0,0 +1,135 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.bot.Volta;
+import org.firstinspires.ftc.teamcode.util.FieldDimensions;
+
+// cycle cones from stack on high goal
+@Autonomous(name="RightStackCycle", group="LeaguePrep")
+public class RightStackCycle extends OpMode {
+
+    // declare bot
+    public Volta bot;
+
+    @Override
+    public void init() {
+
+        // initialize bot
+        bot = new Volta(
+                (FieldDimensions.cellCount - 2) * FieldDimensions.cellSize + FieldDimensions.cellMesh / 2 + Volta.wheelWidth + Volta.frameX / 2,
+                FieldDimensions.cellMesh / 2 + Volta.frameY / 2,
+                0,
+                hardwareMap, telemetry);
+    }
+
+    @Override
+    public void loop() {
+
+        // grab starting cone
+
+        if (bot.next(bot.timer)) {
+            bot.timer.setTarget(Volta.grabWait);
+            bot.claw.close();
+        }
+
+        // navigate to high goal
+
+        if (bot.next(bot.nav)) {
+            bot.nav.setTarget(0, 0, (FieldDimensions.cellCount - 1.5) * FieldDimensions.cellSize, 2 * FieldDimensions.cellSize, 0);
+            bot.slide.setTarget(Volta.restSlide);
+        }
+
+        if (bot.next(bot.nav, bot.slide)) {
+            bot.nav.setTarget(Volta.clawX, Volta.clawY, (FieldDimensions.cellCount - 2) * FieldDimensions.cellSize, 3 * FieldDimensions.cellSize, 0);
+            bot.slide.setTarget(FieldDimensions.highGoal + Volta.aboveSlide);
+        }
+
+        // drop on high goal
+
+        if (bot.next(bot.timer)) {
+            bot.timer.setTarget(Volta.dropBuffer);
+            bot.slide.setTarget(FieldDimensions.highGoal + Volta.belowSlide);
+        }
+
+        if (bot.next(bot.slide)) {
+            bot.claw.open();
+        }
+
+        // cycle from stack
+
+        for (int i = 0; i < 3; i++) {
+
+            // navigate to stack
+
+            if (bot.next(bot.nav)) {
+                bot.nav.setTarget(0, 0, (FieldDimensions.cellCount - 1.5) * FieldDimensions.cellSize, 2.5 * FieldDimensions.cellSize, -Math.PI / 2);
+                bot.slide.setTarget(Volta.restSlide);
+            }
+
+            if (bot.next(bot.nav, bot.slide)) {
+                bot.nav.setTarget(Volta.clawX, Volta.clawY, FieldDimensions.stackX(1), FieldDimensions.stackY(0), -Math.PI / 2);
+                bot.slide.setTarget(FieldDimensions.stackHeight(5 - i));
+            }
+
+            // grab from stack
+
+            if (bot.next(bot.timer)) {
+                bot.timer.setTarget(Volta.grabWait);
+                bot.claw.close();
+            }
+
+            if (bot.next(bot.slide)) {
+                bot.slide.setTarget(FieldDimensions.stackHeight(5) + Volta.aboveSlide);
+            }
+
+            // navigate to high goal
+
+            if (bot.next(bot.nav)) {
+                bot.nav.setTarget(0, 0, (FieldDimensions.cellCount - 1.5) * FieldDimensions.cellSize, 2.5 * FieldDimensions.cellSize, -Math.PI / 2);
+                bot.slide.setTarget(Volta.restSlide);
+            }
+
+            if (bot.next(bot.nav, bot.slide)) {
+                bot.nav.setTarget(Volta.clawX, Volta.clawY, (FieldDimensions.cellCount - 2) * FieldDimensions.cellSize, 3 * FieldDimensions.cellSize, 0);
+                bot.slide.setTarget(FieldDimensions.highGoal + Volta.aboveSlide);
+            }
+
+            // drop on high goal
+
+            if (bot.next(bot.timer)) {
+                bot.timer.setTarget(Volta.dropBuffer);
+                bot.slide.setTarget(FieldDimensions.highGoal + Volta.belowSlide);
+            }
+
+            if (bot.next(bot.slide)) {
+                bot.claw.open();
+            }
+        }
+
+        // park
+
+        if (bot.next(bot.nav, bot.slide)) {
+            bot.nav.setTarget(0, 0, (FieldDimensions.cellCount - 1.5) * FieldDimensions.cellSize, 2.25 * FieldDimensions.cellSize, 0);
+            bot.slide.setTarget(Volta.startSlide);
+        }
+
+        // stop autonomous
+        if (bot.next()) {
+            requestOpModeStop();
+        }
+
+        // update bot
+        if (bot.justInc) bot.timer.reset();
+        bot.update();
+        telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+
+        // stop bot
+        bot.stop();
+    }
+}
